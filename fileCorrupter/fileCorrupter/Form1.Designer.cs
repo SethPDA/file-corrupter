a934e6a65f1c1e97568e7bdbebf5e1d3049e6254
rem2beast <remduece@gmail.com>
2016-05-02 19:26:17 -0400

    Added files via upload

---
+++ fileCorrupter/fileCorrupter/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace fileCorrupter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileToCorrupt = new System.Windows.Forms.Button();
+            this.corrupt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // fileToCorrupt
+            // 
+            this.fileToCorrupt.Location = new System.Drawing.Point(12, 21);
+            this.fileToCorrupt.Name = "fileToCorrupt";
+            this.fileToCorrupt.Size = new System.Drawing.Size(260, 23);
+            this.fileToCorrupt.TabIndex = 0;
+            this.fileToCorrupt.Text = "File To Corrupt";
+            this.fileToCorrupt.UseVisualStyleBackColor = true;
+            this.fileToCorrupt.Click += new System.EventHandler(this.fileToCorrupt_Click);
+            // 
+            // corrupt
+            // 
+            this.corrupt.Location = new System.Drawing.Point(12, 196);
+            this.corrupt.Name = "corrupt";
+            this.corrupt.Size = new System.Drawing.Size(260, 23);
+            this.corrupt.TabIndex = 1;
+            this.corrupt.Text = "Corrupt";
+            this.corrupt.UseVisualStyleBackColor = true;
+            this.corrupt.Click += new System.EventHandler(this.corrupt_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 52);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(41, 117);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(193, 73);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.corrupt);
+            this.Controls.Add(this.fileToCorrupt);
+            this.Name = "Form1";
+            this.Text = "corrupter ultimatecs.xyz";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileToCorrupt;
+        private System.Windows.Forms.Button corrupt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ fileCorrupter/fileCorrupter/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fileCorrupter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void corruptFile(string fileName)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(fileName);
+            byte[] oldBytes = bytes;
+            Random rnd = new Random();
+            int num = rnd.Next(0, 255);
+           
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (bytes[i] % 2 == 0)
+                {
+                    bytes[i] = (byte)rnd.Next(0, (int)(bytes[i] * 1.618 < 256.0 ? bytes[i] * 1.618 : 255));
+                }
+            }
+
+            File.WriteAllBytes(fileName + "1.pdf", bytes);
+        }
+
+        string fileName = "";
+        private void fileToCorrupt_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                fileName = openFileDialog1.FileName;
+                richTextBox1.Text = fileName + " OKAY";
+                
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void corrupt_Click(object sender, EventArgs e)
+        {
+            corruptFile(fileName);
+            richTextBox1.Clear();
+            richTextBox1.Text = "DONE!";
+        }
+    }
+}
