f1b9f063b9f8f1a878720952aca0c2597026eb46
SethPDA <sethpda@live.com>
2021-12-07 15:09:48 +0100

    Added possibility to scramble name of file. Still buggy but works.

--- fileCorrupter/fileCorrupter/Form1.Designer.cs
+++ fileCorrupter/fileCorrupter/Form1.Designer.cs
@@ -32,14 +32,15 @@
             this.corrupt = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // fileToCorrupt
             // 
             this.fileToCorrupt.Location = new System.Drawing.Point(12, 21);
             this.fileToCorrupt.Name = "fileToCorrupt";
-            this.fileToCorrupt.Size = new System.Drawing.Size(260, 23);
+            this.fileToCorrupt.Size = new System.Drawing.Size(641, 73);
             this.fileToCorrupt.TabIndex = 0;
             this.fileToCorrupt.Text = "File To Corrupt";
             this.fileToCorrupt.UseVisualStyleBackColor = true;
@@ -47,12 +48,15 @@
             // 
             // corrupt
             // 
-            this.corrupt.Location = new System.Drawing.Point(12, 196);
+            this.corrupt.BackColor = System.Drawing.Color.Red;
+            this.corrupt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.corrupt.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.corrupt.Location = new System.Drawing.Point(195, 310);
             this.corrupt.Name = "corrupt";
-            this.corrupt.Size = new System.Drawing.Size(260, 23);
+            this.corrupt.Size = new System.Drawing.Size(260, 49);
             this.corrupt.TabIndex = 1;
             this.corrupt.Text = "Corrupt";
-            this.corrupt.UseVisualStyleBackColor = true;
+            this.corrupt.UseVisualStyleBackColor = false;
             this.corrupt.Click += new System.EventHandler(this.corrupt_Click);
             // 
             // openFileDialog1
@@ -61,34 +65,51 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 59);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 100);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(260, 52);
+            this.richTextBox1.Size = new System.Drawing.Size(641, 181);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
-            // richTextBox2
+            // checkBox1
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(41, 117);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(193, 73);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "";
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(237, 287);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Scramble Name and Extension?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.richTextBox2);
+            this.ClientSize = new System.Drawing.Size(665, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.corrupt);
             this.Controls.Add(this.fileToCorrupt);
             this.Name = "Form1";
-            this.Text = "corrupter ultimatecs.xyz";
+            this.Text = "File Corrupter";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,7 +119,8 @@
         private System.Windows.Forms.Button corrupt;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- fileCorrupter/fileCorrupter/Form1.cs
+++ fileCorrupter/fileCorrupter/Form1.cs
@@ -13,18 +13,32 @@ namespace fileCorrupter
 {
     public partial class Form1 : Form
     {
+        bool isScrambleOn;
         public Form1()
         {
             InitializeComponent();
+
+            isScrambleOn = checkBox1.Checked;
         }
 
-        void corruptFile(string fileName)
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
         {
-            byte[] bytes = System.IO.File.ReadAllBytes(fileName);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        string errors;
+        void corruptFile(string fileName, string newFileName)
+        {
+            byte[] bytes = File.ReadAllBytes(fileName);
             byte[] oldBytes = bytes;
             Random rnd = new Random();
             int num = rnd.Next(0, 255);
-           
+
             for (int i = 0; i < bytes.Length; i++)
             {
                 if (bytes[i] % 2 == 0)
@@ -33,18 +47,66 @@ namespace fileCorrupter
                 }
             }
 
-            File.WriteAllBytes(fileName + "1.pdf", bytes);
+            // corrupt file
+            try
+            {
+                File.WriteAllBytes(fileName, bytes);
+            }
+            catch(Exception e)
+            {
+                errors += e;
+            }
+            
+            
+
+            // jumble filename
+            string currentFileName;
+            currentFileName = Path.GetFileName(fileName);
+            string currentFilePath = fileName.Replace(currentFileName, "");
+
+            if (isScrambleOn == true)
+            {
+                //string newFileName = Encoding.Default.GetString(HashString(randomSeed(), 20));
+                string test = currentFilePath + newFileName;
+                //MessageBox.Show(test);
+
+                File.Move(fileName, currentFilePath + newFileName);
+            }
         }
 
-        string fileName = "";
+        string[] fileStrings;
+
         private void fileToCorrupt_Click(object sender, EventArgs e)
         {
+            richTextBox1.Text = "";
+            openFileDialog1.Dispose();
+            List<string> fileList = new List<string>();
+            fileStrings = null;
+
+            openFileDialog1.Multiselect = true;
             DialogResult result = openFileDialog1.ShowDialog();
+            string message = "";
+
+
             if (result == DialogResult.OK) // Test result.
             {
-                fileName = openFileDialog1.FileName;
-                richTextBox1.Text = fileName + " OKAY";
-                
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    //message += file + Environment.NewLine;
+                    //richTextBox1.Text = message;
+                    fileList.Add(file);
+                }
+
+                fileStrings = fileList.ToArray();
+                for (int i = 0; i < fileStrings.Length; i++)
+                {
+                    message += fileStrings[i] + Environment.NewLine;
+                }
+
+                richTextBox1.Text = message;
+                //MessageBox.Show(message);
+
+                //fileName = openFileDialog1.FileName;
             }
         }
 
@@ -55,9 +117,27 @@ namespace fileCorrupter
 
         private void corrupt_Click(object sender, EventArgs e)
         {
-            corruptFile(fileName);
+            foreach (string currentFile in fileStrings)
+            {
+                string newFileName = RandomString(30);
+                corruptFile(currentFile, newFileName);
+            }
+
             richTextBox1.Clear();
-            richTextBox1.Text = "DONE!";
+            richTextBox1.Text = "All files are DONE!";
+
+            MessageBox.Show(errors);
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            isScrambleOn = checkBox1.Checked;
+        }
+
     }
 }
